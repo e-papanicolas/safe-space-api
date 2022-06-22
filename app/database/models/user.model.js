cc791a0d649066cd41e50fcf27441d8d4c7b6de1
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-22 15:26:29 -0400

    updated user model to use sequelize instance passed in

--- app/database/models/user.model.js
+++ app/database/models/user.model.js
@@ -1,33 +1,28 @@
-import { Sequelize, DataTypes } from "sequelize";
-import config from "../config/config.json" assert { type: "json" };
+import { DataTypes } from "sequelize";
 
-const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
-  config.development
-);
+const Users = (sequelize) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [3 - 18],
+      },
+    },
+  });
+  return User;
+};
 
-const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    validate: {
-      len: [3 - 18],
-    },
-  },
-});
-
-export default User;
+export default Users;
