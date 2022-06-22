561780e5927cf94675d0148c94e02cdc28ec7cfe
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-22 16:26:06 -0400

    added migration for profile

---
+++ app/database/migrations/20220622200139-create-profile.cjs
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Profiles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      avatar: {
+        type: Sequelize.STRING,
+      },
+      nickname: {
+        type: Sequelize.STRING,
+      },
+      pronouns: {
+        type: Sequelize.STRING,
+      },
+      country: {
+        type: Sequelize.STRING,
+      },
+      about: {
+        type: Sequelize.STRING,
+      },
+      interests: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Profiles");
+  },
+};
