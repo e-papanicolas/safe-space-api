78d0959e46dec83f6a43db867ecda382b961187d
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-23 09:13:53 -0400

    added profile association to user

--- app/database/models/user.model.js
+++ app/database/models/user.model.js
@@ -1,6 +1,5 @@
 import { DataTypes, Sequelize } from "sequelize";
 import config from "../config/config.json" assert { type: "json" };
-import Profile from "./profile.model.js";
 
 const sequelize = new Sequelize(
   config.development.database,
@@ -9,28 +8,34 @@ const sequelize = new Sequelize(
   config.development
 );
 
-const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    validate: {
-      notEmpty: true,
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [3 - 18],
+      },
     },
   },
-  email: {
-    type: DataTypes.STRING,
-    validate: {
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    validate: {
-      len: [3 - 18],
-    },
-  },
-});
-// User.hasOne(Profile);
-await User.sync({ alter: true });
+  {}
+);
+
+User.associate = (models) => {
+  User.hasOne(models.Profile, { as: "profile" });
+};
 
 export default User;
