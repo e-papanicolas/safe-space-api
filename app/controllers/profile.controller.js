9c3e085e3e03fc37a73991ab4dffca3ba830a52d
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-23 12:24:43 -0400

    initial create profile function

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,8 +1,44 @@
 import { db } from "../database/models/index.js";
 
 const Profile = db.profiles;
+const User = db.users;
 const profileActions = {};
 
+profileActions.new = async (req, res, next) => {
+  try {
+    let userId = req.params.id;
+    let profile = Profile.build({
+      userId: userId,
+      nickname: req.body.nickname,
+    });
+    console.log(profile.user);
+    await profile.save();
+    res.status(200).json(profile);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 profileActions.updateProfile = async (req, res, next) => {
-  
-}
+  try {
+    let user = User.findByPk(req.params.id);
+    let profile = (await Profile.findOne({ where: { user: user.id } })) || null;
+    if (profile) {
+      profileActions.update(req.body);
+    } else {
+      Profile.create(
+        {
+          user: user.id,
+        },
+        req.body
+      );
+    }
+    user.profile = profile;
+    await profile.save();
+    res.status(200).json({ profile, user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export default profileActions;
