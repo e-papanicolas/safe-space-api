7f209413a41a6c639a61ac2c53488d9fd6398722
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-23 12:26:26 -0400

    added new route to profile

--- app/routes/profile.routes.js
+++ app/routes/profile.routes.js
@@ -1,8 +1,12 @@
-import avatarUpload from "../middleware/cloudinary";
+import avatarUpload from "../middleware/cloudinary.js";
 import express from "express";
 import auth from "../middleware/auth.js";
+import profileActions from "../controllers/profile.controller.js";
 
 const profileRoutes = express.Router();
 
 // update profile
-profileRoutes.put("/update", profileActions.updateProfile);
+profileRoutes.put("/:id", profileActions.updateProfile);
+profileRoutes.post("/:id", profileActions.new);
+
+export default profileRoutes;
