bfd6617ad1ca462e6bf020cebca15498960fd3bb
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-24 12:41:00 -0400

    updated find or create method for profile

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,41 +1,27 @@
 import { db } from "../database/models/index.js";
 
 const Profile = db.profiles;
-const User = db.users;
 const profileActions = {};
 
-profileActions.new = async (req, res, next) => {
-  try {
-    let userId = req.params.id;
-    let profile = Profile.build({
-      userId: userId,
-      nickname: req.body.nickname,
-    });
-    console.log(profile);
-    await profile.save();
-    res.status(200).json(profile);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
 profileActions.updateProfile = async (req, res, next) => {
+  const id = req.params.id;
+  const { avatar, nickname, pronouns, country, about, interests } = req.body;
   try {
-    let user = await User.findByPk(req.params.id);
-    let profile = (await Profile.findOne({ where: { user: user.id } })) || null;
-    if (profile) {
-      profileActions.update(req.body);
-    } else {
-      Profile.create(
-        {
-          user: user.id,
-        },
-        req.body
-      );
+    const [profile, created] = await Profile.findOrCreate({
+      where: { id: id },
+      defaults: {
+        userId: id,
+        avatar,
+        nickname,
+        pronouns,
+        country,
+        about,
+        interests,
+      },
+    });
+    if (created) {
+      res.status(200).send({ profile });
     }
-    user.profile = profile;
-    await profile.save();
-    res.status(200).json({ profile, user });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
