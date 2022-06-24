74bde0e9fd92260f0204c756a667cc0a34d0fcc5
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-24 14:42:05 -0400

    completed profile create or update actions

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -4,6 +4,8 @@ const Profile = db.profiles;
 const profileActions = {};
 
 profileActions.updateProfile = async (req, res, next) => {
+  console.log("in profile action");
+  console.log(req.body);
   const id = req.params.id;
   const { avatar, nickname, pronouns, country, about, interests } = req.body;
   try {
@@ -20,8 +22,21 @@ profileActions.updateProfile = async (req, res, next) => {
       },
     });
     if (created) {
-      res.status(200).send({ profile });
+      res.status(200).json({ profile });
+    } else {
+      // it is not created, need to update it
+      await profile.update({
+        userId: id,
+        avatar,
+        nickname,
+        pronouns,
+        country,
+        about,
+        interests,
+      });
     }
+    await profile.save();
+    res.status(200).json({ profile });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
