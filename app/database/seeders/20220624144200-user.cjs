3969322d322e4e02a1913f49ee1cc67c7271a9fb
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-24 11:18:50 -0400

    user seeds created

---
+++ app/database/seeders/20220624144200-user.cjs
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "John_Doe",
+          email: "john@doe.com",
+          password: "password",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    return queryInterface.bulkDelete("People", null, {});
+  },
+};
