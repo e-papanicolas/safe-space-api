e0b785826af30291cc03256a50f6943d4ac68433
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-24 12:41:35 -0400

    working on avatar upload

--- app/middleware/cloudinary.js
+++ app/middleware/cloudinary.js
@@ -18,8 +18,11 @@ const storage = new CloudinaryStorage({
 const parser = multer({ storage: storage });
 
 const avatarUpload = async (req, res, next) => {
+  console.log("in middleware");
   parser.single("image");
+  console.log("after parser");
   const avatar = req.file.path;
+  console.log(avatar);
   req.body.avatar = avatar;
   next();
 };
