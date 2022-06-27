57829cf7fd266dde6877bd511b89e77b012bd5ab
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-27 16:50:09 -0400

    created profiletags with incoming interest string of tag ids

--- app/controllers/profile.controller.js
+++ app/controllers/profile.controller.js
@@ -1,6 +1,8 @@
 import { db } from "../database/models/index.js";
 
 const Profile = db.profiles;
+const Tag = db.tags;
+const ProfileTag = db.profileTags;
 const profileActions = {};
 
 /**
@@ -12,6 +14,15 @@ const profileActions = {};
 profileActions.updateProfile = async (req, res, next) => {
   const id = req.params.id;
   const { avatar, nickname, pronouns, country, about, interests } = req.body;
+  // string: space separated list of tag ids
+  let tagIds = interests.split(" ");
+  await tagIds.forEach(async (tagId) => {
+    return await ProfileTag.create({
+      profileId: id,
+      tagId: tagId,
+    });
+  });
+
   const profileParams = {
     userId: id,
     avatar,
@@ -21,22 +32,50 @@ profileActions.updateProfile = async (req, res, next) => {
     about,
     interests,
   };
+
   try {
-    const [profile, created] = await Profile.findOrCreate({
+    let [profile, created] = await Profile.findOrCreate({
       where: { userId: id },
       defaults: profileParams,
+      include: [
+        {
+          model: ProfileTag,
+          include: [{ model: Tag }],
+        },
+      ],
     });
-    if (created) {
-      return res.status(200).json(profile);
-    } else {
+
+    if (!created) {
       // it is not newly created, need to update it
       await profile.update(profileParams);
+      await profile.save();
     }
-    await profile.save();
-    res.status(200).json({ profile });
+
+    res.status(200).json(profile);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
+
+/**
+ * GET /tags
+ * @function
+ * Returns a list of all tags.
+ */
+profileActions.getTags = async (req, res, next) => {
+  try {
+    const tags = await Tag.findAll();
+    res.status(200).json(Tag.serializeMany(tags));
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/**
+ * POST
+ * @function
+ * This can be used to add new tags.
+ */
+profileActions.addTags = async (req, res, next) => {};
 
 export default profileActions;
