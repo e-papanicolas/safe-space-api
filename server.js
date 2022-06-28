f923c8220c5ad2dd39201bfafd6150625c2cd9e9
Eleni Papanicolas <elenipapanicolas@gmail.com>
2022-06-28 18:02:52 -0400

    added note for production ports

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 import { db } from "./app/database/models/index.js";
 import {
@@ -15,11 +17,16 @@ import {
 const app = express();
 const port = process.env.PORT || 8081;
 
+// FOR PRODUCTION:
+// secure web traffic only allow 443 (https) redirect port 80 traffic (http) to 443
+
+app.use(cookieParser());
 app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(csrf({ cookie: true }));
 
 // use routes
 app.use("/user", userRoutes);
@@ -28,7 +35,7 @@ app.use("/post", postRoutes);
 app.use("/comment", commentRoutes);
 app.use("/report", reportRoutes);
 
-// * change cors configuration to this when running with a front end
+// * change cors configuration to this when running with a front end in a different repo
 // const corsOptions = {origin: "http://localhost:____"};
 // app.use(cors(corsOptions));
 
@@ -40,6 +47,15 @@ app.use("/report", reportRoutes);
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
+
+// app.all("*", function (req, res) {
+//   res.cookie("XSRF-TOKEN", req.csrfToken());
+//   res.render("index");
+// });
+
+// app.get("/getCSRFToken", (req, res) => {
+//   res.json({ csrfToken: req.csrfToken() });
+// });
 
 app.get("/", (req, res, next) => {
   res.send("Hello World!");
